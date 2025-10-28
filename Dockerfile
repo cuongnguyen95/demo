a1a639da6635501ae8b017876c6ec86ff4a01b6a
cuongnv32@tnexfinance.com.vn <cuongnv32@tnexfinance.com.vn>
2025-10-28 08:33:34 +0700

    test

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@
 FROM eclipse-temurin:17-jdk
 
 # Copy file jar từ target vào container
-COPY target/demo-0.0.1.jar app.jar
+COPY target/demo-0.0.2.jar app.jar
 
 # Expose cổng ứng dụng
-EXPOSE 8081
+EXPOSE 8084
 
 # Lệnh chạy Spring Boot
 ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "User", description = "User management APIs")
 public class UserController {
 
+    private static final Logger log = LogManager.getLogger(UserController.class);
+
     @GetMapping("/{id}")
     @Operation(summary = "Get user by ID", description = "Return user information")
     public String getUser(@PathVariable Long id) {
-        return "hehehe";
+        log.info("Get user by ID{}", id);
+        log.info("hehehehe");
+        return "Cuong vua sua day";
     }
 }
